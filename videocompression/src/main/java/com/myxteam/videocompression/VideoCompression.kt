de12b7c65aae8e36cd6272d3eb1f844b53dbb3a0
vsoft-phuong <vsoftphuong@gmail.com>
2021-06-10 10:13:49 +0700

    - Audio strategy - Mute option

--- videocompression/src/main/java/com/myxteam/videocompression/VideoCompression.kt
+++ videocompression/src/main/java/com/myxteam/videocompression/VideoCompression.kt
@@ -7,7 +7,9 @@ import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.otaliastudios.transcoder.Transcoder
 import com.otaliastudios.transcoder.TranscoderListener
 import com.otaliastudios.transcoder.common.TrackType
+import com.otaliastudios.transcoder.strategy.DefaultAudioStrategy
 import com.otaliastudios.transcoder.strategy.DefaultVideoStrategy
+import com.otaliastudios.transcoder.strategy.RemoveTrackStrategy
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -19,7 +21,8 @@ class VideoCompression(
     var progressIndicator: LinearProgressIndicator? = null,
     var framerate: Int = 24,
     var height: Int = 960,
-    var width: Int = 540
+    var width: Int = 540,
+    var isMute: Boolean = false
 ) {
     fun showProgress() {
         progressIndicator?.run {
@@ -36,20 +39,29 @@ class VideoCompression(
         }
     }
 
-    lateinit var strategy: DefaultVideoStrategy
+    lateinit var videoStrategy: DefaultVideoStrategy
+    lateinit var audioStrategy: DefaultAudioStrategy
 
     init {
         showProgress()
-        strategy = DefaultVideoStrategy.exact(height, width)
+
+        videoStrategy = DefaultVideoStrategy.exact(height, width)
             .frameRate(framerate)
             .build()
+
+        audioStrategy = DefaultAudioStrategy.builder()
+            .channels(DefaultAudioStrategy.CHANNELS_AS_INPUT)
+            .sampleRate(DefaultAudioStrategy.SAMPLE_RATE_AS_INPUT)
+            .build()
+
     }
 
     fun compress() {
         var compressed = createVideoFile(context)
         Transcoder.into(compressed.absolutePath)
             .addDataSource(TrackType.VIDEO, videoPath)
-            .setVideoTrackStrategy(strategy)
+            .setVideoTrackStrategy(videoStrategy)
+            .setAudioTrackStrategy(if (isMute) audioStrategy else RemoveTrackStrategy())
             .setListener(object : TranscoderListener {
                 override fun onTranscodeProgress(progress: Double) {
                     val number = (progress * 100).toInt()
